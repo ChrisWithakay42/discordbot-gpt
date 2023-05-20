5b24a4080f2e9fed021626e6230628bf1b7a50b9
ChrisWithakay42 <codewithakay@gmail.com>
2023-05-20 12:36:51 +0100

    setting pup authenticated session for boto

--- cicd.py
+++ cicd.py
@@ -1,7 +1,29 @@
 import boto3
+from environs import Env
+
+env = Env()
+env.read_env()
+
+session = boto3.Session(
+    aws_access_key_id=env.str('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=env.str('AWS_SECRET_ACCESS_KEY'),
+    region_name='eu-west-2'
+)
 
 
 class AwsSecretManagerManager:
 
     def __init__(self, client):
         self.client = client
+
+
+class AwsApiGatewayManager:
+
+    def __init__(self):
+        ...
+
+
+class AwsLambdaManager:
+
+    def __init__(self):
+        ...
